f62abe7bb4881dd6f3d185da799f07a5f2a5b05b
kimvnhung <kimvnhung@gmail.com>
2019-05-06 22:14:18 +0700

    initial

---
+++ app/src/main/java/com/hung/kimvan/bluetoothwithmicrobit/AdvanceFragment.java
@@ -0,0 +1,23 @@
+package com.hung.kimvan.bluetoothwithmicrobit;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class AdvanceFragment extends Fragment {
+    Button btnAdvanced;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.advanced_fragment,container,false);
+
+
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/hung/kimvan/bluetoothwithmicrobit/BasicFragment.java
@@ -0,0 +1,22 @@
+package com.hung.kimvan.bluetoothwithmicrobit;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class BasicFragment extends Fragment {
+    Button btnBasic;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.basic_fragment,container,false);
+
+
+        return view;
+    }
+}
